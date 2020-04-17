5cd7bb4c4c0b5107767debb51d4802d7279cf1f5
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2020-04-17 13:30:20 +0530

    clean up

--- MailBox/Program.fs
+++ MailBox/Program.fs
@@ -1,4 +1,4 @@
-﻿open Menu
+﻿open System
 open MailUtils
 open Hopac
 open Logary
@@ -8,6 +8,41 @@ open Logary.Targets
 
 [<EntryPoint>]
 let main _argv =
+
+    let menuOptions = [ "Send Mail"; "Read Mail" ]
+
+    let sendMailQuestions =
+        [ "Enter valid recipient mailId's(Comma or semicolon Seperated, if multiple):";
+        "Enter valid from MailId:";
+        "Enter mail subject:";
+        "Enter mail body:" ]
+
+    let receiveMailQuestions = [ "Enter Server Name:"; "Enter Username:"; "Enter Password" ]
+
+    let printOptions options = options |> List.iteri (fun i x -> Console.WriteLine("{0}. {1}", (i + 1), x))
+
+    let getOption n =
+        Console.Write("Enter option:")
+        List.iter (fun (i: int) ->
+            Console.Write((if i = n then "{0}" else "{0},"), i)) [ 1 .. n ]
+        Console.WriteLine()
+        Console.ReadLine().Trim()
+
+    let mainMenu() =
+        Console.WriteLine("Menu:")
+        menuOptions |> printOptions
+        menuOptions.Length |> getOption
+    
+    let getUserInput (message: string) =
+        Console.WriteLine(message)
+        Console.ReadLine().Trim()
+
+    let retrieveAnswers questions =
+        seq {
+            for question in questions do
+                yield getUserInput question
+        }
+
     let logary =
         Config.create "Mailbox-Core" "local"
         |> Config.target (LiterateConsole.create LiterateConsole.empty "console")
